4a9190ea6a893cbc28e518b9a177c3de10614a66
Tom Henighan <henighan.2@gmail.com>
2019-01-27 19:00:23 -0800

    updated training to include evaluation of policy based on Q. Its a bit messy tho

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import common
+from collections import defaultdict
 
 
 def play_agent_hand(agent_cards, dealer_up_card, Q, deck, epsilon=0.1):
@@ -40,12 +41,17 @@ def update_Q_and_counter(
     return Q, counter
 
 
-def train(n_episodes, gamma=0.9, epsilon=0.1):
+def train(n_episodes, gamma=0.9, epsilon=0.1, evaluation_episodes=None,
+          n_evaluate_episodes=None):
     toc = time.time()
     Q = 0.1*np.random.rand(len(common.AGENT_HANDS), 10, len(common.ACTIONS))
     rewards = np.zeros(n_episodes)
     counter = np.zeros_like(Q)
     deck = common.initialize_deck()
+    evaluation = defaultdict(list)
+    eval_eps_copy = evaluation_episodes[:]
+    eval_eps_copy.sort(reverse=True)
+    eval_episode = eval_eps_copy.pop()
     for ii in range(n_episodes):
         if len(deck) < 16:
             deck = common.initialize_deck()
@@ -54,9 +60,17 @@ def train(n_episodes, gamma=0.9, epsilon=0.1):
         rewards[ii] = reward
         Q, counter = update_Q_and_counter(
             Q, counter, agent_state_action_pairs, reward, gamma=gamma)
+        if ii == eval_episode:
+            mean_reward, err = common.evaluate_strategy(
+                Q, n_episodes=n_evaluate_episodes)
+            evaluation['mean_reward'].append(mean_reward)
+            evaluation['95%'].append(err)
+            evaluation['episode_no'].append(ii)
+            if eval_eps_copy:
+                eval_episode = eval_eps_copy.pop()
     print('MONTE CARLO training {} episodes took {} seconds'.format(
         n_episodes, (time.time() - toc)))
-    return Q, rewards
+    return Q, rewards, evaluation
 
 
 if __name__=='__main__':
