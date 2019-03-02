b049d8452a503a7f811b177d24e8c24e43234506
Tom Henighan <henighan.2@gmail.com>
2019-03-02 11:48:04 -0800

    missed setup py in push

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __version__ = '1.0'
 setup(
     name='blackjack-rl',
     version=__version__,
-    packages=['blackjack'],
+    packages=['blackjack', 'blackjack.learners'],
     install_requires=[
         'numpy',
         'matplotlib',
